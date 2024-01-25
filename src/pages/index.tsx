8520b5c880a7c23690fde175d740f939c8f4b661
ueni.tech <ueni.tech@gmail.com>
2024-01-25 22:38:37 +0900

    #1 未完・完了の切り替え動作実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,29 @@ import AddButton from "./componets/AddButton";
 import { useState } from 'react';
 
 export default function Home() {
-  const [incompleteTodos, setInconpleteTodos] = useState(["買い物に行く", "確定申告に行く", "ドットインストールを進める"]);
-  const [completeTodos, setCompleteTodos] = useState(["Reactの復習", "Discordの招待", "MENTAでチャットの返信"]);
+  const [incompleteTodos, setInconpleteTodos] = useState([{id: 1, title: "買い物にいく"}, {id: 2, title: "確定申告をする"}, {id: 3, title: "ドットインストールを進める"}]);
+  const [completedTodos, setCompletedTodos] = useState([{id: 4, title: "Discordの招待"}, {id: 5, title: "MENTAでチャットの返信"}, {id: 6, title: "Reactの復習"}]);
+
+
+  const onChangeCompleted = (index: number) => {
+    const newIncompleteTodos = [...incompleteTodos];
+    newIncompleteTodos.splice(index, 1);
+
+    const newCompletedTodos = [...completedTodos, incompleteTodos[index]];
+
+    setInconpleteTodos(newIncompleteTodos);
+    setCompletedTodos(newCompletedTodos);
+  }
+
+  const onChangeBack = (index: number) => {
+    const newCompletedTodos = [...completedTodos];
+    newCompletedTodos.splice(index, 1);
+
+    const newIncompleteTodos = [...incompleteTodos, completedTodos[index]];
+
+    setInconpleteTodos(newIncompleteTodos);
+    setCompletedTodos(newCompletedTodos);
+  }
 
   return (
     <>
@@ -13,13 +34,13 @@ export default function Home() {
         <Box w="400px">
           <Heading size="md">2024年1月24日</Heading>
           <Box w="100%" bg="blackAlpha.50" p={5}>
-            
+
             <Box>
               <Heading size="sm" mb={5}>未完了</Heading>
               <UnorderedList display="flex" flexDirection="column" gap={5}>
-                {incompleteTodos.map(incompleteTodo => (
-                  <ListItem listStyleType="none">
-                    <Checkbox>{incompleteTodo}</Checkbox>
+                {incompleteTodos.map((incompleteTodo, index) => (
+                  <ListItem listStyleType="none" key={incompleteTodo.id}>
+                    <Checkbox onChange={() => onChangeCompleted(index)}>{incompleteTodo.title}</Checkbox>
                   </ListItem>
                 ))}
               </UnorderedList>
@@ -28,9 +49,9 @@ export default function Home() {
             <Box mt={5}>
               <Heading size="sm" mb={5}>完了</Heading>
               <UnorderedList display="flex" flexDirection="column" gap={5}>
-                {completeTodos.map(completeTodo => (
-                  <ListItem listStyleType="none">
-                    <Checkbox>{completeTodo}</Checkbox>
+                {completedTodos.map((completeTodo, index) => (
+                  <ListItem listStyleType="none" key={completeTodo.id}>
+                    <Checkbox defaultChecked onChange={()=>onChangeBack(index)}>{completeTodo.title}</Checkbox>
                   </ListItem>
                 ))}
               </UnorderedList>
